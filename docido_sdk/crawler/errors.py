af419fa18194f431be486db75b8016ef1cf731b0
Tristan Carel <tristan@cogniteev.com>
2015-08-05 12:03:27 +0200

    Rename crawler 'exceptions' to 'errors' + new error class
    Crawler can now raise an exception to notify that the OAuth token has to be refreshed

--- docido_sdk/crawler/errors.py
+++ docido_sdk/crawler/errors.py
@@ -1,4 +1,9 @@
 
+"""
+Provides a set of Exception classes a crawler may raise when it is
+not possible to consume source API because of OAuth-related issues.
+"""
+
 class CrawlerError(Exception):
     """Common base exception class for crawl issues"""
     def __init__(self, message=None):
@@ -32,3 +37,16 @@ class OAuthTokenPermanentError(CrawlerError):
           the error message returned by source API.
         """
         super(OAuthTokenPermanentError, self).__init__(message)
+
+
+class OAuthTokenRefreshRequiredError(CrawlerError):
+    """Exception class used to notify crawler manager when
+    the OAuth token of an account has expired or will expire
+    shortly, and need to be refreshed programmaticaly.
+    """
+    def __init__(self, message=None):
+        """
+        @param message: optional printable object providing
+          the error message returned by source API.
+        """
+        super(OAuthTokenRefreshRequiredError, self).__init__(message)
