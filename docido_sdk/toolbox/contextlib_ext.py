e3780b2751aac7a1a0c261b4b058aaff855b8e8b
Tristan Carel <tristan@cogniteev.com>
2015-08-28 15:03:09 +0200

    Apply `restore' utility to dictionary only

--- docido_sdk/toolbox/contextlib_ext.py
+++ docido_sdk/toolbox/contextlib_ext.py
@@ -4,16 +4,19 @@ import copy
 
 
 @contextmanager
-def restore(obj, copy_func=copy.deepcopy):
+def restore_dict_kv(a_dict, key, copy_func=copy.deepcopy):
     """Backup an object in a with context and restore it when leaving
     the scope.
 
-    :param obj: object to backup
+    :param a_dict:
+      associative table
+    :param: key
+      key whose value has to be backed up
     :param copy_func: callbable object used to create an object copy.
     default is `copy.deepcopy`
     """
-    backup = copy_func(obj)
+    backup = copy_func(a_dict[key])
     try:
-        yield obj
+        yield
     finally:
-        obj = backup
+        a_dict[key] = backup
