ee0308696af83f8bd75db7b5b717d9e7a0ae70f2
Tristan Carel <tristan@cogniteev.com>
2015-11-09 17:17:43 +0100

    Crawler can now override max concurrent task parameter

--- docido_sdk/crawler/api.py
+++ docido_sdk/crawler/api.py
@@ -37,13 +37,22 @@ class ICrawler(Interface):
 
         - 'tasks' (mandatory): generator of :py:func:`functools.partial` tasks
           to execute to perform the account synchronization.
-          partial objects may accept 3 arguments:
+          partial objects may accept 4 arguments:
 
               - push_api (:py:class:`docido_sdk.push.IndexAPI`)
               - oauth_token (:py:class:`docido_sdk.oauth.OAuthToken`)
               - previous_result (:py:class:`object`) previous task result,
                 if any.
               - logger (:py:class:`logging.Logger`)
+
+          If a list of tasks is returned, then they are executed concurrently,
+          and the tasks order is not honored.
+          If a list of list of tasks is returned, then every list is executed
+          concurrently and task order among a list is respected. the
+          `prev_result` parameter of a sub-task will be given what the
+          previous sub-task of the same list returns. `None` is given to the
+          first task of every sequence.
+
 
         - 'epilogue' (optional): a :py:func:`functools.partial` instance
           to execute when all sub-tasks have been executed. The partial
@@ -54,6 +63,10 @@ class ICrawler(Interface):
               - results (a result or a list of results)
               providing what the sub-tasks returned.
               - logger (:py:class:`logging.Logger`)
+
+        - 'max_concurrent_tasks' (optional): an integer greater than 0 and less
+        than 10 (the default value) used to limit number of tasks executed
+        concurrently.
 
         A task cannot be instance, class, or static method
         of a :py:func:`docido_sdk.core.Component` object.
