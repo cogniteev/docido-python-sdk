b66f6ac5f6ccc26645cfd7493ab8328e0b64bfce
Tristan Carel <tristan@cogniteev.com>
2016-03-23 11:55:49 +0100

    Bump yamlious to 0.2.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'pytz>=2015.6',
         'setuptools>=0.6',
         'six>=1.10.0',
-        'yamlious>=0.2',
+        'yamlious>=0.2.1',
     ],
     entry_points="""
         [console_scripts]
