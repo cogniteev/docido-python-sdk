c45093a57f852056463a05a91cd9a13bffd096b9
Tristan Carel <tristan@cogniteev.com>
2016-03-29 16:00:52 +0200

    Bump python-dateutil to 2.5.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'elasticsearch==1.6.0',
         'numpy==1.11.0',
         'ProxyTypes==0.9',
-        'python-dateutil>=2.4.2',
+        'python-dateutil>=2.5.2',
         'pytz>=2015.6',
         'requests>=2.8.1',
         'setuptools>=0.6',
