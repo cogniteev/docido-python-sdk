965c06645c13ddaea31c5a7a0595f3c8e7604885
Tristan Carel <tristan@cogniteev.com>
2016-04-14 23:28:05 +0200

    Does not numpy to compute levenshtein distance
    * Heavy dependency to carry
    * Make it very hard to build a debian package containing all Docido deps

--- docido_sdk/toolbox/text.py
+++ docido_sdk/toolbox/text.py
@@ -1,7 +1,4 @@
-
 import os
-
-import numpy as np
 
 
 def to_unicode(text, charset=None):
@@ -54,51 +51,34 @@ def exception_to_unicode(e, traceback=False):
     return message
 
 
-def levenshtein(source, target):
+def levenshtein(s, t):
     """ Compute the Levenshtein distance between 2 strings, which
     is the minimum number of operations required to perform on a string to
     get another one.
 
     code taken from https://en.wikibooks.org
 
-    :param basestring source:
-    :param basestring source:
+    :param basestring s:
+    :param basestring t:
     :rtype: int
     """
-    if len(source) < len(target):
-        return levenshtein(target, source)
+    ''' From Wikipedia article; Iterative with two matrix rows. '''
+    if s == t:
+        return 0
+    elif len(s) == 0:
+        return len(t)
+    elif len(t) == 0:
+        return len(s)
+    v0 = [None] * (len(t) + 1)
+    v1 = [None] * (len(t) + 1)
+    for i in range(len(v0)):
+        v0[i] = i
+    for i in range(len(s)):
+        v1[0] = i + 1
+        for j in range(len(t)):
+            cost = 0 if s[i] == t[j] else 1
+            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+        for j in range(len(v0)):
+            v0[j] = v1[j]
 
-    # So now we have len(source) >= len(target).
-    if len(target) == 0:
-        return len(source)
-
-    # We call tuple() to force strings to be used as sequences
-    # ('c', 'a', 't', 's') - numpy uses them as values by default.
-    source = np.array(tuple(source))
-    target = np.array(tuple(target))
-
-    # We use a dynamic programming algorithm, but with the
-    # added optimization that we only need the last two rows
-    # of the matrix.
-    previous_row = np.arange(target.size + 1)
-    for s in source:
-        # Insertion (target grows longer than source):
-        current_row = previous_row + 1
-
-        # Substitution or matching:
-        # Target and source items are aligned, and either
-        # are different (cost of 1), or are the same (cost of 0).
-        current_row[1:] = np.minimum(
-            current_row[1:],
-            np.add(previous_row[:-1], target != s)
-        )
-
-        # Deletion (target grows shorter than source):
-        current_row[1:] = np.minimum(
-            current_row[1:],
-            current_row[0:-1] + 1
-        )
-
-        previous_row = current_row
-
-    return previous_row[-1]
+    return v1[len(t)]
