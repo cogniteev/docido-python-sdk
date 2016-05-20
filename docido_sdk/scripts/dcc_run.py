fb0d75b4825b579932fefcc38bfaa1b6fed4f09d
Tristan Carel <tristan@cogniteev.com>
2016-05-20 18:48:58 +0200

    dcc_run: expose more code used by docido_contrib_crawlers

--- docido_sdk/scripts/dcc_run.py
+++ docido_sdk/scripts/dcc_run.py
@@ -8,9 +8,6 @@ import pickle
 from pickle import PickleError
 import shutil
 import sys
-import time
-
-import six
 
 from .. import loader
 from ..env import env
@@ -21,7 +18,7 @@ from ..core import (
     ExtensionPoint,
 )
 from ..crawler import ICrawler
-from ..crawler.errors import Retry
+from ..crawler.run import TasksRunner
 from ..index.config import YamlPullCrawlersIndexingConfig
 from ..index.processor import (
     Elasticsearch,
@@ -29,32 +26,7 @@ from ..index.processor import (
 )
 from docido_sdk.index.pipeline import IndexPipelineProvider
 import docido_sdk.config as docido_config
-from ..toolbox.collections_ext import Configuration, nameddict
-from ..toolbox.date_ext import timestamp_ms
-from ..crawler.tasks import (
-    reorg_crawl_tasks,
-    split_crawl_tasks,
-)
-
-
-def wait_or_raise(logger, retry_exc, attempt):
-    if attempt == retry_exc.max_retries:
-        raise retry_exc
-    if retry_exc.countdown is not None:
-        assert isinstance(retry_exc.countdown, six.integer_types)
-        wait_time = retry_exc.countdown
-        if wait_time < 0:
-            raise (Exception("'countdown' is less than 0"), None,
-                   sys.exc_info()[2])
-    else:
-        assert isinstance(retry_exc.eta, datetime.datetime)
-        target_ts = timestamp_ms.feeling_lucky(retry_exc.eta)
-        now_ts = timestamp_ms.now()
-        wait_time = (target_ts - now_ts) / 1e3
-        if wait_time < 0:
-            raise Exception("'eta' is in the future"), None, sys.exc_info()[2]
-    logger.warn("Retry raised, waiting {} seconds".format(wait_time))
-    time.sleep(wait_time)
+from ..toolbox.collections_ext import Configuration
 
 
 def oauth_tokens_from_file():
@@ -96,68 +68,9 @@ class LocalRunner(Component):
             index_api = index_provider.get_index_api(
                 self.service, None, None
             )
-            attempt = 1
-            while True:
-                try:
-                    crawl_config = nameddict(config.get('config') or {})
-                    crawl_config.setdefault('full', False)
-                    tasks = crawler.iter_crawl_tasks(
-                        index_api, config.token,
-                        crawl_config, logger,
-                    )
-                    break
-                except Retry as e:
-                    try:
-                        wait_or_raise(logger, e, attempt)
-                    except:
-                        logger.exception('Max retries reached')
-                        raise
-                    else:
-                        attempt += 1
-                except Exception:
-                    logger.exception('Unexpected exception was raised')
-                    raise
-
-            self._check_pickle(tasks)
-            tasks, epilogue, concurrency = reorg_crawl_tasks(
-                tasks,
-                int(config.get('max_concurrent_tasks', 2))
-            )
-            tasks = split_crawl_tasks(tasks, concurrency)
-
-            def _runtask(task, prev_result):
-                attempt = 1
-                result = None
-                kwargs = dict()
-                while True:
-                    try:
-                        result = task(index_api, config.token, prev_result,
-                                      crawl_config, logger, **kwargs)
-                        break
-                    except Retry as e:
-                        try:
-                            wait_or_raise(logger, e, attempt)
-                        except:
-                            logger.exception('Max retries reached')
-                            result = e
-                            break
-                        else:
-                            attempt += 1
-                            kwargs = e.kwargs
-                    except Exception as e:
-                        logger.exception('Unexpected exception was raised')
-                        result = e
-                        break
-                return result
-
-            results = []
-            for seq in tasks:
-                previous_result = None
-                for task in seq:
-                    previous_result = _runtask(task, previous_result)
-                results.append(previous_result)
-            if epilogue is not None:
-                _runtask(epilogue, results)
+            runner = TasksRunner(crawler, index_api, config, logger)
+            self._check_pickle(runner.tasks)
+            runner.execute()
         return {
             'service': self.service,
             'name': self.launch,
@@ -206,7 +119,6 @@ class LocalRunner(Component):
                 )
                 yield self.run(logger, config, c)
 
-
 DEFAULT_OUTPUT_PATH = osp.join(os.getcwd(), '.dcc-runs')
 
 
@@ -214,30 +126,15 @@ def parse_options(args=None):
     if args is None:  # pragma: no cover
         args = sys.argv[1:]
     parser = ArgumentParser()
-    parser.add_argument(
-        '-i', '--incremental',
-        metavar='PATH',
-        help='trigger incremental crawl'
-    )
-    parser.add_argument(
-        '-o', '--output',
-        metavar='PATH',
-        default=DEFAULT_OUTPUT_PATH,
-        help='Override persisted data, [default=%(default)s]'
-    )
-    parser.add_argument(
-        '-v', '--verbose',
-        action='count',
-        dest='verbose',
-        help='set verbosity level',
-        default=0
-    )
-    parser.add_argument(
-        'crawls',
-        metavar='CRAWL',
-        nargs='*',
-        help='Sub-set of crawls to launch'
-    )
+    parser.add_argument('-i', '--incremental', metavar='PATH',
+                        help='trigger incremental crawl')
+    parser.add_argument('-o', '--output', metavar='PATH',
+                        default=DEFAULT_OUTPUT_PATH,
+                        help='Override persisted data, [default=%(default)s]')
+    parser.add_argument('-v', '--verbose', action='count', dest='verbose',
+                        help='set verbosity level', default=0)
+    parser.add_argument('crawls', metavar='CRAWL', nargs='*',
+                        help='Sub-set of crawls to launch')
 
     return parser.parse_args(args)
 
